4fecf09aaf26b7b5f6de2bfaf37dbd3c39dcf610
Iryna Munchak <iryna.munchak@ukma.edu.ua>
2023-11-03 17:59:51 +0200

    Add form

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
