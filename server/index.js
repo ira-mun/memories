dbe666e943dba37ab9ad2148ab82ff5e686fba8a
Iryna Munchak <iryna.munchak@ukma.edu.ua>
2023-11-03 21:40:27 +0200

    finish project

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 
 const app = express();
-
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -15,9 +15,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ira_mun:4dOC8PKMpiVMy8De@cluster0.o3ksj.mongodb.net/';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
